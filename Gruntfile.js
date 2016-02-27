ee1bf8b8fc98be79b4618233271069be14ff0f18
st3ffane <steph.ponteins@gmail.com>
2016-02-27 14:30:35 +0100

    grunt
    fichiers générés via grunt -1er essai

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+
+  // Configuration de Grunt
+  grunt.initConfig({
+        concat: {
+              options: {
+                separator: ';', // permet d'ajouter un point-virgule entre chaque fichier concaténé.
+              },
+              dist: {
+                src: ['src/datas/objects.js',
+                       'src/datas/array.js',
+                       'src/bindings/property_binding.js',
+                       'src/bindings/textcontent_binding.js',
+                       'src/bindings/attribute_binding.js',
+                       'src/bindings/model_binding.js',
+                       'src/bindings/array_binding.js',
+                       'src/bindings/command_binding.js',
+                       'src/bindings/input_binding.js',
+                       'src/bindings/extras/webservice_binding.js',
+                       'src/utils.js',
+                        'src/ftw2.js'],
+                dest: 'output/concat.js'
+              }
+        },
+        umd: {
+                all: {
+                    options: {
+                        src: 'output/concat.js',
+                        dest: 'output/ftw2.js', // optional, if missing the src will be used
+                        
+                    }
+                }
+        },
+    
+        uglify: {
+                options: {
+                        separator: ';'
+                      },
+              dist: {
+                src: ['output/ftw2.js'],
+                dest: 'output/ftw2-min.js'
+              }
+        }
+  })
+
+  // Définition des tâches Grunt
+  grunt.loadNpmTasks('grunt-contrib-concat'); // Voilà l'ajout.
+  grunt.loadNpmTasks('grunt-contrib-uglify'); // Voilà l'ajout.
+  grunt.loadNpmTasks('grunt-umd'); // Voilà l'ajout.
+  
+  grunt.registerTask('default', ['concat:dist','umd:all','uglify:dist'])
+
+}
