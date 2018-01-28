637016890fec761621c5e5b10076ca17ae765b2c
st3ffane <steph.ponteins@gmail.com>
2018-01-28 11:20:01 +0100

    second commit

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,14 +1,11 @@
 // global css
 import './theme/theme.scss';
-import {initScene, start} from './app/scenery';
 
 /**
- * entrance code for SPA
+ * Demo app for FTW2
  */
 function main() {
-    const scenery = initScene();
-    document.body.appendChild(scenery.renderer.domElement);
-    start(scenery);
+    console.log('started!');
 }
 
 document.addEventListener('DOMContentLoaded', main);
